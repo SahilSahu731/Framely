4003586a6b78ff77051ad21a13f4984a7fa86abc
Sahil Sahu <sahilsahu731709@gmail.com>
2025-08-01 19:06:12 +0530

    make some changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -17,7 +17,7 @@ export const registerUser = async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
@@ -46,7 +46,7 @@ export const loginUser = async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
